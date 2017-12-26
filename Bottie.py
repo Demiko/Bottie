5d073fda24f227341e5a61ee7b21a54fa1ac1254
Demiko <demiko@gmail.com>
2017-12-26 23:10:29 +0200

    Added "roll" command; revised "halt"

--- Bottie.py
+++ Bottie.py
@@ -1,5 +1,6 @@
 import discord
-from discord import channel
+import random
+import re
 
 
 class Bottie(discord.Client):
@@ -7,14 +8,26 @@ class Bottie(discord.Client):
         super().__init__(**options)
 
     async def on_message(self, message:discord.Message):
-        idTag = '<@%s>' % message.server.me.id
-        if not message.content.startswith(idTag):
+        if not self.user.mentioned_in(message):
             return
-
-        text:str = message.content.replace(idTag, '').strip()
+        text:str = message.content.replace(self.user.mention, '').strip()
         print(text)
         if text == "halt":
-            await self.send_message(message.channel, content='Bye-bye')
-            await self.logout()
+            info = await self.application_info()
+            if message.author.id == info.owner.id:
+                await self.send_message(message.channel, content='Bye-bye.')
+                await self.logout()
+            else:
+                await self.send_message(message.channel, content="Only my master can do that!")
+            return
+        elif text.startswith("roll"):
+            params = re.fullmatch(r'([0-9]*)d([0-9]+)', text[5:])
+            if params:
+                rolls = int(params[1]) if params[1] else 1
+                sides = int(params[2])
+                responce = "I've rolled %s. Result is %s" % (params[0], sum(random.choices(range(sides), k=rolls)))
+                await self.send_message(message.channel, content=responce)
+            else:
+                await self.send_message(message.channel, content="Sorry. I can't roll that.")
         else:
-            await self.send_message(message.channel, content='I\'m alive')
+            await self.send_message(message.channel, content='I\'m alive!')
