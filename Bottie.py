4711110732ca7576d293cc8e246e7bdcb9ac25bc
Demiko <demiko18@gmail.com>
2017-12-27 13:34:19 +0200

    fix catching incorrect roll

--- Bottie.py
+++ Bottie.py
@@ -22,20 +22,25 @@ class Bottie(discord.Client):
             response = "Hi, my name is %s." % (self.appInfo.name)
         elif re.match("roll", text):
             roll = re.match(r"roll (?P<rolls>\d+)?d(?P<sides>\d+)(?P<modifier>[\+-]\d+)?", text)
-            rolls = int(roll.group('rolls')) if roll.group('rolls') else 1
-            sides = int(roll.group('sides'))
-            modifier = int(roll.group('modifier')) if roll.group('modifier') else 0
-            if rolls == 0 or sides == 0:
-                response = "It's %s, and you know this!" % modifier
-            elif rolls > 100:
-                response = "Chill! I have only 100 dice."
-            elif sides > 100:
-                response = "Chill! I have only d100 max."
-            elif sides == 1:
-                response = "Obviously, it's %s!" % (rolls + modifier)
+            if not roll:
+                response = "I can't roll that."
             else:
-                result = random.choices(range(1, sides+1), k=rolls)
-                response = "I've rolled %s. Result is %s %s." % (roll[0][5:], sum(result) + modifier, result)
+                rolls = int(roll.group('rolls')) if roll.group('rolls') else 1
+                sides = int(roll.group('sides'))
+                modifier = int(roll.group('modifier')) if roll.group('modifier') else 0
+                if rolls < 0 or sides < 0:
+                    response = "You know, I would have to go to negative universe to do that. Try something more sane"
+                elif rolls == 0 or sides == 0:
+                    response = "It's %s, and you know this!" % modifier
+                elif rolls > 100:
+                    response = "Chill! I have only 100 dice."
+                elif sides > 100:
+                    response = "Chill! I have only d100 max."
+                elif sides == 1:
+                    response = "Obviously, it's %s!" % (rolls + modifier)
+                else:
+                    result = random.choices(range(1, sides+1), k=rolls)
+                    response = "I've rolled %s. Result is %s %s." % (roll[0][5:], sum(result) + modifier, result)
         elif re.match("halt", text):
             if message.author.id == self.appInfo.owner.id:
                 await self.logout(message.channel)
