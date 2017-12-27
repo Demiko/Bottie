1054ef4c74ac0ee261609096d344e6673eb9475a
Demiko <demiko18@gmail.com>
2017-12-27 10:43:22 +0200

    Updated "roll" command

--- Bottie.py
+++ Bottie.py
@@ -20,14 +20,25 @@ class Bottie(discord.Client):
             else:
                 await self.send_message(message.channel, content="Only my master can do that!")
             return
-        elif text.startswith("roll"):
-            params = re.fullmatch(r'([0-9]*)d([0-9]+)', text[5:])
+        if text.startswith("roll"):
+            params = re.fullmatch(r'([0-9]*)d([0-9]+)()', text[5:])
             if params:
                 rolls = int(params[1]) if params[1] else 1
                 sides = int(params[2])
-                responce = "I've rolled %s. Result is %s" % (params[0], sum(random.choices(range(sides), k=rolls)))
-                await self.send_message(message.channel, content=responce)
+                if rolls == 0 or sides == 0:
+                    response = "It's zero, and you know it! :angry:"
+                elif rolls > 100:
+                    response = "Chill! I have only 100 dice."
+                elif sides > 100:
+                    response = "Chill! I have only d100 max."
+                elif sides == 1:
+                    response = "Obviously, it's %s!" % (rolls)
+                else:
+                    roll = random.choices(range(1, sides+1), k=rolls)
+                    response = "I've rolled %s. Result is %s %s." % (params[0], sum(roll), roll)
+                response = "%s, %s" % (message.author.mention, response)
+                await self.send_message(message.channel, content=response)
             else:
                 await self.send_message(message.channel, content="Sorry. I can't roll that.")
-        else:
-            await self.send_message(message.channel, content='I\'m alive!')
+            return
+        await self.send_message(message.channel, content='Command not recognized. Sorry.')
